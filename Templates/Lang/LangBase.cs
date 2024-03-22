bf4fd7e7e2861602a4e847deeca4ebbb90982061
채일문 <jjack000@naver.com>
2024-03-22 15:36:06 +0900

    multi lang 로직 추가

---
+++ Templates/Lang/LangBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewIP
+{
+    public class LangBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        protected bool kr;
+        public void Korean()
+        {
+            kr = true;
+            Update();
+        }
+
+        public void English()
+        {
+            kr = false;
+            Update();
+        }
+
+        public void Update()
+        {
+            // 모든 property를 업데이트
+            for (int i = 0; i < GetType().GetProperties().Length; i++)
+            {
+                OnPropertyChanged(GetType().GetProperties()[i].Name);
+            }
+        }
+    }
+}
---
+++ Templates/Lang/MainWindow_Lang.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace NewIP
+{
+	public class MainWindowLang : LangBase
+	{
+		public string File => kr ? "파일" : "File";
+		public string Language => kr ? "언어 변경" : "Language";
+		public string Cascade => kr ? "계단식 구성" : "Cascade";
+		public string Tile_Horizontal => kr ? "가로 타일 구성" : "Tile_Horizontal";
+		public string Tile_Vertical => kr ? "세로 타일 구성" : "Tile_Vertical";
+		public string Arrange_Icons => kr ? "아이콘 정렬" : "Arrange_Icons";
+		public string Windows => kr ? "창 설정" : "Windows";
+		
+	}
+}
+
+
